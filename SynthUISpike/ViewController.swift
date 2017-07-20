beec04e1e44324aa736b3e119daf5aeaad4436b5
Matthew Michael <matthew.fecher@gmail.com>
2017-07-20 13:37:17 -0400

    Initial Commit

---
+++ SynthUISpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SynthUISpike
+//
+//  Created by Matthew Fecher on 7/20/17.
+//  Copyright © 2017 Matthew Fecher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
