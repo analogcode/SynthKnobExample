815e5aea972228bcb5073b51445b338d526b5d84
Matthew Michael <matthew.fecher@gmail.com>
2017-07-21 17:04:13 -0400

    init commit

---
+++ KnobView.swift
@@ -0,0 +1,21 @@
+//
+//  KnobView.swift
+//  SynthUISpike
+//
+//  Created by Matthew Fecher on 7/20/17.
+//  Copyright © 2017 Matthew Fecher. All rights reserved.
+//
+
+import UIKit
+
+class KnobView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
